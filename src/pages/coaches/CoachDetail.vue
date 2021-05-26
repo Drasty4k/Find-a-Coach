7670748542a101706f01a98a63b736ea7d63baec
DragosDev <barbuta.dragos12@gmail.com>
2021-05-26 12:31:28 +0300

    built

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <section>
+            <base-card>
+                <h2>{{ fullName }}</h2>
+                <h3>${{ rate }}/hour</h3>
+            </base-card>
+        </section>
+        <section>
+            <base-card>
+                <header>
+                    <h2>Interested? Reach out now!</h2>
+                    <base-button v-if="contactButtonVisibility" @click="buttonVisibility" link :to="contactLink">Contact</base-button>
+                </header>
+                <router-view></router-view>
+            </base-card>
+        </section>
+        <section>
+            <base-card>
+                <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
+                <p>{{ description }}</p>
+            </base-card>
+        </section>
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['id'],
+    data() {
+        return {
+            selectedCoach: null,
+            contactButtonVisibility: true
+        }
+    },
+    methods: {
+        buttonVisibility() {
+            if (this.$route.path.includes(this.id)){
+                this.contactButtonVisibility = false
+            }
+        }
+    },
+    computed: {
+        fullName() {
+            return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName 
+        },
+        areas() {
+            return this.selectedCoach.areas
+        },
+        rate() {
+            return this.selectedCoach.hourlyRate
+        },
+        description() {
+            return this.selectedCoach.description
+        },
+        contactLink() {
+            if (this.$route.path.includes('contact')) {
+                return this.$route.path
+            } else
+                return this.$route.path + '/contact';
+                
+            },
+            
+        },
+    
+    created() {
+        this.selectedCoach = this.$store.getters['coaches/coaches']
+                                                .find(coach => coach.id === this.id)
+    }
+}
+</script>
